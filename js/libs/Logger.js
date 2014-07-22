fd1ab7a398a1c3de11ec00f7b65dcd9903063452
zhoushineyoung <zhoushineyoung@hotmail.com>
2014-07-22 20:55:39 +0800

    logger edit

--- js/libs/Logger.js
+++ js/libs/Logger.js
@@ -5,6 +5,18 @@ var log4js = require('log4js');
 log4js.configure('../../config/log4js_config.json', {});
 
 exports.logger=function(){
+    var logger = log4js.getLogger("relative-logger");
+    logger.setLevel('INFO');
+    return logger;
+};
+
+exports.logger2=function(){
+    var logger = log4js.getLogger("absolute-logger");
+    logger.setLevel('INFO');
+    return logger;
+};
+
+exports.consoleLogger=function(){
     var logger = log4js.getLogger("console");
     logger.setLevel('INFO');
     return logger;
--- js/libs/Server.js
+++ js/libs/Server.js
@@ -3,7 +3,10 @@
  */
 var http = require('http');
 var logger = require('./Logger').logger();
+var logger2 = require('./Logger').logger2();
+var consoleLogger = require('./Logger').consoleLogger();
 var url = require("url");
+
 function service(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
@@ -12,7 +15,9 @@ function service(route, handle) {
         route(handle, pathname, response);
     }
     http.createServer(onRequest).listen(8089, '127.0.0.1');
-    logger.info("Server has started.");
-    console.log("Server has started.");
+    logger.info('Server has started.');
+    logger2.info('hass');
+    consoleLogger.info('mylogger');
+    console.log('Server has started.');
 }
 exports.service = service;
